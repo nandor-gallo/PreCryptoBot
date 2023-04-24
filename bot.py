ec10a869504195b60678a00dc110e8020f5a0f7d
Nandor Gallo <nandor.gallo@uoit.net>
2023-04-24 01:12:09 -0400

    Discord bot init

---
+++ bot.py
@@ -0,0 +1,28 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+
+intent = discord.Intents.default()
+client = discord.Client(intents=intent)
+
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+client.run(TOKEN)
