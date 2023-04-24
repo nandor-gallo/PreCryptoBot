1b5ab70a176ce6a7258b828c6606345cd3ffee8a
Nandor Gallo <nandor.gallo@uoit.net>
2023-04-24 16:12:11 -0400

    Responds to DMS

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -9,20 +10,44 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 
-intent = discord.Intents.default()
-client = discord.Client(intents=intent)
+intent = discord.Intents
+intents = [discord.Intents.dm_messages, discord.Intents.dm_typing, intent.guild_messages, intent.guilds, ]
 
+
+
+client = discord.Client(intents=(discord.Intents.default()))
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
+    guild = discord.utils.get(client.guilds, name=GUILD)
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Playground!'
+    )
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    
 client.run(TOKEN)
