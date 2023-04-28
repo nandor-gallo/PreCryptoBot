45e0970f821261a14d17205211fa6174d983d9c7
Nandor Gallo <nandor.gallo@uoit.net>
2023-04-28 13:09:46 -0400

    Added 2 bot commands

--- bot.py
+++ bot.py
@@ -36,6 +36,9 @@ async def on_ready():
 # Listen for the $price command and retrieve the latest Bitcoin price from CoinMarketCap
 @client.event
 async def on_message(message):
+    if message.content.startswith('$help'):
+        await message.channel.send("```Here are a list of commands you can use: \n\n$price [symbol] - find out latest price of crpyto\n$change [symbol] - find out latest coin changes within 24H 7D 30D```")
+
     if message.content.startswith('$price'):
         # Split the user's message into tokens and extract the desired cryptocurrency name
         tokens = message.content.split()
@@ -58,6 +61,31 @@ async def on_message(message):
             crypto_data = data['data'][crypto_name.upper()]
             crypto_price = crypto_data['quote']['USD']['price']
             await message.channel.send(f"{crypto_name.upper()} price: ${crypto_price:.8f}")
+
+    if message.content.startswith('$change'):
+        # Split the user's message into tokens and extract the desired cryptocurrency name
+        tokens = message.content.split()
+        if len(tokens) < 2:
+            await message.channel.send('Please specify a cryptocurrency name')
+            return
+        crypto_name = tokens[1]
+
+        # Make an API call to CoinMarketCap to retrieve the latest cryptocurrency data
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+        params = {'symbol': crypto_name.upper()}
+        headers = {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': API_KEY}
+        response = requests.get(url, headers=headers, params=params)
+        data = json.loads(response.text)
+
+        # Extract the latest cryptocurrency price and send it back to the user
+        if 'status' in data and data['status']['error_code'] == 400:
+            await message.channel.send(f'Error: {data["error"]["error_message"]}')
+        else:
+            crypto_data = data['data'][crypto_name.upper()]
+            crypto_change1D = crypto_data['quote']['USD']['percent_change_24h']
+            crypto_change7D = crypto_data['quote']['USD']['percent_change_7d']
+            crypto_change30D = crypto_data['quote']['USD']['percent_change_30d']
+            await message.channel.send(f"{crypto_name.upper()} changes after [ 24H {crypto_change1D:.2f}% | 7 Days {crypto_change7D:.2f}% | 30 Days {crypto_change30D}]")
 
 # Run the bot
 client.run(TOKEN)
